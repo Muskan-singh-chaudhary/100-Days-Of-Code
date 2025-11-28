b30c971f904feaea438f9899178bec6efece03ae
Muskan Chaudhary <muskan.upes.stu@gmail.com>
2025-11-28 10:48:13 +0530

    Update Day-72-Of-Code.c

--- Day-72-Of-Code.c
+++ Day-72-Of-Code.c
@@ -1,42 +1,27 @@
-Q143: Find and print student with highest marks
+Q122: Read file contents using fgets()
 #include <stdio.h>
 
-struct Student {
-    char name[50];
-    int roll_no;
-    float marks;
-};
-
 int main() {
-    struct Student students[5];
-    int i, max_index = 0;
+    FILE *file;
+    char line[200];
     
-    printf("Enter details of 5 students:\n");
+    file = fopen("info.txt", "r");
     
-    for (i = 0; i < 5; i++) {
-        printf("\nStudent %d:\n", i + 1);
-        
-        printf("Name: ");
-        scanf("%s", students[i].name);
-        
-        printf("Roll Number: ");
-        scanf("%d", &students[i].roll_no);
-        
-        printf("Marks: ");
-        scanf("%f", &students[i].marks);
-    }
-   
-    for (i = 1; i < 5; i++) {
-        if (students[i].marks > students[max_index].marks) {
-            max_index = i;
-        }
+    if (file == NULL) {
+        printf("Error: Could not open file!\n");
+        return 1;
     }
     
-    printf("\n\nStudent with Highest Marks:\n");
-    printf("Name: %s\n", students[max_index].name);
-    printf("Roll Number: %d\n", students[max_index].roll_no);
-    printf("Marks: %.2f\n", students[max_index].marks);
+    printf("Contents of info.txt:\n");
+    printf("---------------------\n");
+    
+    while (fgets(line, sizeof(line), file) != NULL) {
+        printf("%s", line);
+    }
+    
+    printf("---------------------\n");
+    
+    fclose(file);
     
     return 0;
 }
-
