9875adaa421eb32128814b267df4de72f6ca58ea
Muskan Chaudhary <muskan.upes.stu@gmail.com>
2025-11-28 10:47:28 +0530

    Update Day-70-Of-Code.c

--- Day-70-Of-Code.c
+++ Day-70-Of-Code.c
@@ -1,76 +1,33 @@
-Q139: Show that enums store integers
+Q120: Convert string to sentence case
 #include <stdio.h>
-
-enum Priority {
-    LOW = 1,
-    MEDIUM = 5,
-    HIGH = 10,
-    CRITICAL = 20
-};
+#include <string.h>
 
 int main() {
-    enum Priority p1 = LOW;
-    enum Priority p2 = HIGH;
+    char str[200];
+    int i;
+    int capitalize_next = 1;
     
-    printf("Enums are stored as integers:\n");
-    printf("LOW = %d\n", LOW);
-    printf("MEDIUM = %d\n", MEDIUM);
-    printf("HIGH = %d\n", HIGH);
-    printf("CRITICAL = %d\n", CRITICAL);
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
     
-    printf("\nVariable p1 = %d\n", p1);
-    printf("Variable p2 = %d\n", p2);
-    
-    printf("\nArithmetic operations:\n");
-    printf("p1 + p2 = %d\n", p1 + p2);
-    printf("p2 - p1 = %d\n", p2 - p1);
-    
-    return 0;
-}
-
-Q140: Struct with enum Gender
-#include <stdio.h>
-
-enum Gender {
-    MALE,
-    FEMALE,
-    OTHER
-};
-
-struct Person {
-    char name[50];
-    int age;
-    enum Gender gender;
-};
-
-int main() {
-    struct Person person;
-    
-    printf("Enter name: ");
-    scanf("%s", person.name);
-    
-    printf("Enter age: ");
-    scanf("%d", &person.age);
-    
-    printf("Enter gender (0=MALE, 1=FEMALE, 2=OTHER): ");
-    scanf("%d", &person.gender);
-    
-    printf("\nPerson Details:\n");
-    printf("Name: %s\n", person.name);
-    printf("Age: %d\n", person.age);
-    printf("Gender: ");
-    
-    switch(person.gender) {
-        case MALE:
-            printf("Male\n");
-            break;
-        case FEMALE:
-            printf("Female\n");
-            break;
-        case OTHER:
-            printf("Other\n");
-            break;
+    printf("Original: %s", str);
+   
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n') {
+            capitalize_next = 1;
+        } else if (capitalize_next && str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32;
+            capitalize_next = 0;
+        } else if (!capitalize_next && str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] + 32;
+        } else if (str[i] >= 'a' && str[i] <= 'z') {
+            capitalize_next = 0;
+        } else if (str[i] >= 'A' && str[i] <= 'Z') {
+            capitalize_next = 0;
+        }
     }
+    
+    printf("Sentence case: %s", str);
     
     return 0;
 }
