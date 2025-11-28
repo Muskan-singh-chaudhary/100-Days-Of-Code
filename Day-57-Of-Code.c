08cc00799f5feeb8a9ab30f1bd2b96e354c7661a
Muskan Chaudhary <muskan.upes.stu@gmail.com>
2025-11-28 10:34:57 +0530

    Update Day-57-Of-Code.c

--- Day-57-Of-Code.c
+++ Day-57-Of-Code.c
@@ -1,86 +1,52 @@
-Q113: Find kth smallest element in array
+Q107: Find previous greater element for each element (brute force)
 #include <stdio.h>
 
 int main() {
-    int n, k, i, j;
+    int n, i, j;
     
     printf("Enter size of array: ");
     scanf("%d", &n);
     
     int arr[n];
+    int prev_greater[n];
     
     printf("Enter array elements:\n");
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
     
-    printf("Enter k (for kth smallest): ");
-    scanf("%d", &k);
-    
     printf("Array: ");
     for (i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     printf("\n");
-
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    
-    printf("Sorted array: ");
+   
     for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    
-    if (k > 0 && k <= n) {
-        printf("%dth smallest element: %d\n", k, arr[k - 1]);
-    } else {
-        printf("Invalid k value\n");
-    }
-    
-    return 0;
-}
-
-Q114: Length of longest substring without repeating characters
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char s[100];
-    int i, j, k;
-    int max_length = 0;
-    
-    printf("Enter a string: ");
-    scanf("%s", s);
-    
-    int len = strlen(s);
-    printf("String: %s\n", s);
-  
-    for (i = 0; i < len; i++) {
-        int visited[256] = {0};
-        
-        for (j = i; j < len; j++) {
-            if (visited[(int)s[j]] == 1) {
+        prev_greater[i] = -1; 
+      
+        for (j = i - 1; j >= 0; j--) {
+            if (arr[j] > arr[i]) {
+                prev_greater[i] = arr[j];
                 break;
             }
-            
-            visited[(int)s[j]] = 1;
-            int current_length = j - i + 1;
-            
-            if (current_length > max_length) {
-                max_length = current_length;
-            }
+        }
+        
+        printf("For element %d at index %d: ", arr[i], i);
+        if (prev_greater[i] != -1) {
+            printf("Previous greater element is %d\n", prev_greater[i]);
+        } else {
+            printf("No previous greater element\n");
         }
     }
     
-    printf("Length of longest substring without repeating characters: %d\n", max_length);
+    printf("\nPrevious Greater Elements: ");
+    for (i = 0; i < n; i++) {
+        printf("%d", prev_greater[i]);
+        if (i < n - 1) {
+            printf(", ");
+        }
+    }
+    printf("\n");
     
     return 0;
 }
