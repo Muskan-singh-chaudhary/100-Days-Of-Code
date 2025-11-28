0f6f89156ec05eb3c3a13bb6a2c355a4fb77f4f2
Muskan Chaudhary <muskan.upes.stu@gmail.com>
2025-11-28 10:05:12 +0530

    Create Day-54-Of-Code.c

---
+++ Day-54-Of-Code.c
@@ -0,0 +1,97 @@
+Q107: Find previous greater element for each element (brute force)
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    int prev_greater[n];
+    
+    printf("Enter array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+ 
+    for (i = 0; i < n; i++) {
+        prev_greater[i] = -1;  
+   
+        for (j = i - 1; j >= 0; j--) {
+            if (arr[j] > arr[i]) {
+                prev_greater[i] = arr[j];
+                break;
+            }
+        }
+        
+        printf("For element %d at index %d: ", arr[i], i);
+        if (prev_greater[i] != -1) {
+            printf("Previous greater element is %d\n", prev_greater[i]);
+        } else {
+            printf("No previous greater element\n");
+        }
+    }
+    
+    printf("\nPrevious Greater Elements: ");
+    for (i = 0; i < n; i++) {
+        printf("%d", prev_greater[i]);
+        if (i < n - 1) {
+            printf(", ");
+        }
+    }
+    printf("\n");
+    
+    return 0;
+}
+
+Q108: Product of array except self
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    
+    int nums[n];
+    int answer[n];
+    
+    printf("Enter array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+    
+    printf("Array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+  
+    for (i = 0; i < n; i++) {
+        int product = 1;
+      
+        for (j = 0; j < n; j++) {
+            if (i != j) {
+                product *= nums[j];
+            }
+        }
+        
+        answer[i] = product;
+        printf("Product except nums[%d] (%d): %d\n", i, nums[i], answer[i]);
+    }
+    
+    printf("\nAnswer array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", answer[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
